fe60745a677a0d583a659ff9036c4d2cb61f16d4
seokjin8678 <seokjin8678@gmail.com>
2023-08-11 21:28:04 +0900

    fix: 주소 수정

--- blog-config.js
+++ blog-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "Festago",
   description: "2023 우아한테코코스 5기 페스타고 팀의 공식 블로그 입니다.",
   author: "Festago",
-  siteUrl: "https://festago-official.github.io/",
+  siteUrl: "https://festago.github.io/",
   links: {
     github: "https://github.com/woowacourse-teams/2023-festa-go",
   },
